557c113f8434a87a9dacbe5c7f991ab5e9c111ae
Sasan Dilantha <sasandilantha12@gmail.com>
2025-03-25 20:09:11 +0530

    code complete

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -5,14 +5,24 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <AHT10.h>
+#include <Servo.h>
+#include <FS.h>
+#include <SPIFFS.h>
 
 #define WIFI_SSID "Xperia_3309"
 #define WIFI_PASSWORD "ds516dila"
 #define SERVER_URL "http://192.168.64.44:5000"
+#define FAN_PIN 5
+#define HIGH_TEMP_THRESHOLD 30.0
+#define LOW_TEMP_THRESHOLD 25.0
+#define FEED_GATE_PIN 6
 
 AHT10 aht(AHT10_ADDRESS_0X38, AHT10_SENSOR);
 MQ135 mq135_sensor(34);
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
+Servo feedGate;
+bool manualFanControl = false;
+bool fanStatus = false;
 
 void connectWiFi() {
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -40,6 +50,24 @@ void initDisplay() {
     display.clearDisplay();
 }
 
+void controlFan(float temp) {
+    if (!manualFanControl) {
+        if (temp >= HIGH_TEMP_THRESHOLD) {
+            digitalWrite(FAN_PIN, HIGH);
+            fanStatus = true;
+        } else if (temp <= LOW_TEMP_THRESHOLD) {
+            digitalWrite(FAN_PIN, LOW);
+            fanStatus = false;
+        }
+    }
+}
+
+void toggleFan(bool state) {
+    manualFanControl = state;
+    digitalWrite(FAN_PIN, state ? HIGH : LOW);
+    fanStatus = state;
+}
+
 String sendData(float temp, float humidity, float airQuality) {
     HTTPClient http;
     http.begin(String(SERVER_URL) + "/data");
@@ -48,7 +76,8 @@ String sendData(float temp, float humidity, float airQuality) {
     String jsonPayload = "{";
     jsonPayload += "\"temperature\":" + String(temp) + ",";
     jsonPayload += "\"humidity\":" + String(humidity) + ",";
-    jsonPayload += "\"airQuality\":" + String(airQuality);
+    jsonPayload += "\"airQuality\":" + String(airQuality) + ",";
+    jsonPayload += "\"fanStatus\":" + String(fanStatus ? "true" : "false");
     jsonPayload += "}";
 
     int httpResponseCode = http.POST(jsonPayload);
@@ -62,21 +91,59 @@ void showData(float temp, float humidity, float airQuality, String message) {
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
-    display.printf("Temp: %.2f C\nHumidity: %.2f %%\nAir Q: %.2f PPM\nMsg: %s", temp, humidity, airQuality, message.c_str());
+    display.printf("Temp: %.2f C\nHumidity: %.2f %%\nAir Q: %.2f PPM\nFan: %s\nMsg: %s", 
+                   temp, humidity, airQuality, fanStatus ? "ON" : "OFF", message.c_str());
     display.display();
+}
+
+void checkFeedTime() {
+    File file = SPIFFS.open("/feed_time.txt", "r");
+    if (!file) {
+        Serial.println("Failed to open feed_time.txt");
+        return;
+    }
+
+    String feedTime = file.readStringUntil('\n');
+    file.close();
+    feedTime.trim();
+
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+
+    char currentTime[6];
+    snprintf(currentTime, sizeof(currentTime), "%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min);
+
+    if (feedTime.equals(String(currentTime))) {
+        Serial.println("Feeding time! Opening feed gate...");
+        feedGate.write(90);
+        delay(15000);
+        Serial.println("Closing feed gate...");
+        feedGate.write(0);
+    }
 }
 
 void setup() {
     Serial.begin(115200);
+    pinMode(FAN_PIN, OUTPUT);
+    digitalWrite(FAN_PIN, LOW);
+    feedGate.attach(FEED_GATE_PIN);
     connectWiFi();
     initSensors();
     initDisplay();
+    if (!SPIFFS.begin(true)) {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+    }
 }
 
 void loop() {
     float temp = aht.readTemperature();
     float humidity = aht.readHumidity();
     float airQuality = mq135_sensor.getCorrectedPPM(temp, humidity);
+    controlFan(temp);
+    checkFeedTime();
     String message = sendData(temp, humidity, airQuality);
     showData(temp, humidity, airQuality, message);
     delay(10000);
